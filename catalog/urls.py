aec6ac100ab62b816a1fa400672f59758cbf70e5
Elena Koltyrina <geek.albis@gmail.com>
2024-06-09 18:46:26 +0200

    Добавленны данные БД, и выполнена 1-я миграция

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,10 @@
-
 from django.urls import path
 
-from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
 
-app_name = CatalogConfig.name
+app_name = 'catalog'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
+    path('contacts/', contacts, name='contacts')
 ]
