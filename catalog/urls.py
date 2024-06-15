849db160c5b79f30e31b04830f28908552bd9769
Elena Koltyrina <geek.albis@gmail.com>
2024-06-15 19:46:40 +0200

    Файлы html разделены, сделаны карточки, для продуктов

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import index, product_details, products, contacts
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', index),
+    path('products/', products, name='products'),
+    path('products/<int:pk>/', product_details, name='product_details'),
     path('contacts/', contacts, name='contacts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Product, Contact
 
 
-def home(request):
-    # Сортируем по дате создания и берем первые 3 товара с помощью -created_at
-    latest_products = Product.objects.order_by('-created_at')[:3]
-
-    # Выводим названия товаров в консоль
-    for product in latest_products:
-        print(product.name)
-
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'base.html')
 
 
 def contacts(request):
-    contact = Contact.objects.all()  # Получаем все объекты из модели Contact
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
+        Contact.objects.create(name=name, phone=phone, message=message)
+        return render(request, 'contacts.html', {'message': 'Ваше сообщение отправлено!'})
+    return render(request, 'contacts.html')
 
-    return render(request, 'contacts.html', {'contacts': contact})
+
+def products(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'products.html', context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_details.html', context)
