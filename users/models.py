a124e0323778fa69b7ee305cb5db047da4e941ea
Elena Koltyrina <geek.albis@gmail.com>
2024-07-12 18:25:49 +0200

    регистрация, вход и выход

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    phone = models.CharField(max_length=35, verbose_name='Номер телефона', **NULLABLE)
+    country = models.CharField(max_length=255, verbose_name='Страна', **NULLABLE, help_text='Например: Россия')
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', **NULLABLE, help_text='Максимальный размер 2Мб')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import UserRegisterForm
+from .models import User
+
+
+class UserCreateView(CreateView):
+    """Создание нового пользователя."""
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
